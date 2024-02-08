a458ad2d64ae04ea6edc4248bdb516ab01777ef7
Shourya Bose <sbose@gpulogin2.lcrc.anl.gov>
2024-02-08 08:28:18 -0600

    final

--- runner.sh
+++ runner.sh
@@ -2,13 +2,12 @@
 #SBATCH --job-name=HETERO
 #SBATCH --output=HET.log
 #SBATCH --account=NEXTGENOPT    
-#SBATCH --partition=gpu
+#SBATCH --partition=gpu-large
 #SBATCH --gres=gpu:8
 #SBATCH --time=23:59:59
-#SBATCH --ntasks=26
+#SBATCH --mem=2000GB
 
-now=$(date)
-echo $now
+
 nvidia-smi
 
 cd ~/FederatedPersonalizedLoadForecasting
@@ -17,15 +16,26 @@ export ENVN="APPFLENV"
 export STATE="IL"
 export STATE2="NY"
 export PATH="/home/sbose/.latex/bin/x86_64-linux:${PATH}"
+NUM_PROC=4
 
 module load anaconda3
 source activate $ENVN
 conda info
 
-# srun -n 13 --cpus-per-task 4 python train.py --state $STATE --choice_local 0
-# srun -n 13 --cpus-per-task 4 python train.py --state $STATE --choice_local 1
-srun -n 13 --cpus-per-task 10 python train.py --state $STATE --choice_local 2
-srun -n 13 --cpus-per-task 10 python train.py --state $STATE2 --choice_local 2
-# srun -n 13 --cpus-per-task 4 python train.py --state $STATE --choice_local 3
+srun --exclusive --ntasks 13 --cpus-per-task $NUM_PROC --mem-per-cpu 36GB python train.py --state $STATE --choice_local 0
+srun --exclusive --ntasks 13 --cpus-per-task $NUM_PROC --mem-per-cpu 36GB python train.py --state $STATE --choice_local 1
+srun --exclusive --ntasks 13 --cpus-per-task $NUM_PROC --mem-per-cpu 36GB python train.py --state $STATE --choice_local 2
+srun --exclusive --ntasks 13 --cpus-per-task $NUM_PROC --mem-per-cpu 36GB python train.py --state $STATE --choice_local 3
+wait
+
+srun --exclusive --ntasks 13 --cpus-per-task $NUM_PROC --mem-per-cpu 36GB python train.py --state $STATE2 --choice_local 0
+srun --exclusive --ntasks 13 --cpus-per-task $NUM_PROC --mem-per-cpu 36GB python train.py --state $STATE2 --choice_local 1
+srun --exclusive --ntasks 13 --cpus-per-task $NUM_PROC --mem-per-cpu 36GB python train.py --state $STATE2 --choice_local 2
+srun --exclusive --ntasks 13 --cpus-per-task $NUM_PROC --mem-per-cpu 36GB python train.py --state $STATE2 --choice_local 3
+wait
+
 cp train.py "experiments${STATE}/train.py"
 cp train.sh "experiments${STATE}/train.sh"
+
+cp train.py "experiments${STATE2}/train.py"
+cp train.sh "experiments${STATE2}/train.sh"
